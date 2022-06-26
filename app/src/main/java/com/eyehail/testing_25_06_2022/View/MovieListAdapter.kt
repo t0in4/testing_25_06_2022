be724ccc9240dcbc1459f0078e27067436241392
t0in4 <t0in4@yahoo.com>
2022-06-26 18:26:07 +0300

    debug

--- app/src/main/java/com/eyehail/testing_25_06_2022/View/MovieListAdapter.kt
+++ app/src/main/java/com/eyehail/testing_25_06_2022/View/MovieListAdapter.kt
@@ -9,21 +9,28 @@ import com.eyehail.testing_25_06_2022.MainActivity
 import com.eyehail.testing_25_06_2022.Model.Movie
 import com.eyehail.testing_25_06_2022.Model.Result
 import com.eyehail.testing_25_06_2022.R
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.movie_item.view.*
 
-class MovieListAdapter(private val movies: ArrayList<Movie>) :
+class MovieListAdapter(private val movies: ArrayList<Result>) :
     RecyclerView.Adapter<MovieListAdapter.DataViewHolder>() {
         class DataViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-            fun bind(movie: Movie) {
+            fun bind(movie: Result) {
 
-                itemView.movieName.text = movie.results.firstOrNull()?.display_title
-                itemView.movieReview.text = movie.results.firstOrNull()?.summary_short
-                val url = movie.results.firstOrNull()?.multimedia?.src
+                itemView.movieName.text = movie.display_title
+                itemView.movieReview.text = movie.summary_short
+                val url = movie.multimedia.src
                 Glide.with(itemView.movieImage.context)
                     .load(url)
                     .into(itemView.movieImage)
             }
         }
+/*    var gson = Gson()
+    var data = gson.fromJson(movies, Array<Movie>::class.java)
+    for (x in 0..data.size) {
+        println(data[x].results)
+    }*/
+
 
     override fun getItemCount() = movies.size
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = DataViewHolder (
@@ -34,11 +41,11 @@ class MovieListAdapter(private val movies: ArrayList<Movie>) :
         holder.bind(movies[position])
     }
 
-    fun newMovies(newMovies: Movie) {
+    fun newMovies(newMovies: List<Result>) {
         movies.clear()
-        movies.addAll(listOf(newMovies))
+        movies.addAll(newMovies)
         //this.notifyItemInserted(movies.size-1)
-        this.notifyDataSetChanged()
+        notifyDataSetChanged()
     }
 
 
